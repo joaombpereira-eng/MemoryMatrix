aeebff452caabdbba03ef6276c33810a2a0083aa
João Pereira <joaopereira.eng.tec@gmail.com>
2020-11-27 15:19:33 +0000

    Adds to the MemoryMatrix app an implementation of HistoryActivity using ListView

--- app/src/main/java/edu/isel/pdm/memorymatrix/GameRepository.kt
+++ app/src/main/java/edu/isel/pdm/memorymatrix/GameRepository.kt
@@ -1,6 +1,8 @@
 package edu.isel.pdm.memorymatrix
 
 import android.content.SharedPreferences
+import android.os.AsyncTask
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import edu.isel.pdm.memorymatrix.game.data.MatrixPattern
@@ -39,23 +41,35 @@ class GameRepository(
                 .apply()
         }
 
+    /**
+     * Saves the given game result in the history DB and, if it's the case, registers it as an
+     * high score.
+     *
+     * @param toGuess   the original pattern
+     * @param guesses   the player's guesses
+     * @param score     the number of correct guesses
+     */
     fun saveResult(toGuess: MatrixPattern, guesses: MatrixPattern, score: Int) {
         worker.execute {
-            db.getGameResultsDao().insertGame(
-                GameResult(
-                    side = toGuess.side,
-                    toGuess = toGuess.toList(),
-                    guesses = guesses.toList(),
-                    score = score,
-                    date = Date()
-                )
-            )
+            Log.v("GameRepository", "Executing Saving Result on thread ${Thread.currentThread().name}")
+            db.getGameResultsDao().insertGame(GameResult(
+                side = toGuess.side,
+                toGuess = toGuess.toList(),
+                guesses = guesses.toList(),
+                score = score,
+                date = Date()
+            ))
         }
-
+        Log.v("GameRepository", "Scheduled saving Result on thread ${Thread.currentThread().name}")
         if (score == toGuess.count && score > highestLevel)
             highestLevel = score
     }
 
+    /**
+     * Asynchronously fetches all existing scored (an unrealistic approach)
+     *
+     * @return the LiveData instance that will contain the data once the async operation is completed
+     */
     fun getAllScores(): LiveData<List<GameResult>> {
         val results = MutableLiveData<List<GameResult>>()
         worker.execute {
@@ -64,6 +78,12 @@ class GameRepository(
         return results
     }
 
+    /**
+     * Asynchronously fetches the most recent [count] results
+     *
+     * @param count the maximum number of results to be produced
+     * @return the LiveData instance that will contain the data once the async operation is completed
+     */
     fun getNScores(count: Int): LiveData<List<GameResult>> =
         db.getGameResultsDao().loadLastGames(count)
 }
