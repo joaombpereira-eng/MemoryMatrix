6d3f94e0db43defe48a4f8f4acf723bfe4521831
João Pereira <joaopereira.eng.tec@gmail.com>
2020-11-28 18:08:20 +0000

    Adds code written during session 12

---
+++ app/src/main/java/edu/isel/pdm/memorymatrix/game/history/HistoryActivity.kt
@@ -0,0 +1,66 @@
+package edu.isel.pdm.memorymatrix.game.history
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.isel.pdm.memorymatrix.R
+import edu.isel.pdm.memorymatrix.utils.BaseActivity
+
+/**
+ * The recyclerview´s view holder
+ */
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val topText: TextView = itemView.findViewById(R.id.historyItemTopText)
+    val bottomText: TextView = itemView.findViewById(R.id.historyItemBottomText)
+}
+
+/**
+ * The recyclerview´s adapter
+ */
+class HistoryAdapter(
+        resources: Resources,
+        private val history: List<GameResult>
+    ) : RecyclerView.Adapter<ItemViewHolder>() {
+
+    private val topFormatString = resources.getString(R.string.history_item_score)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+        return ItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.topText.text = String.format(topFormatString, history[position].score.toString())
+        holder.bottomText.text = history[position].date.toString()
+    }
+
+    override fun getItemCount() = history.size
+}
+
+/**
+ * Screen used to display the game scores' history using a Recycler View.
+ */
+class HistoryActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        val history = findViewById<RecyclerView>(R.id.historyView)
+        history.setHasFixedSize(true)
+        history.layoutManager = LinearLayoutManager(this)
+
+        val viewModel: HistoryViewModel by viewModels()
+
+        viewModel.results.observe(this) {
+            history.adapter = HistoryAdapter(resources, it)
+        }
+    }
+}
---
+++ app/src/main/java/edu/isel/pdm/memorymatrix/game/history/HistoryActivityLegacy.kt
@@ -0,0 +1,65 @@
+package edu.isel.pdm.memorymatrix.game.history
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.viewModels
+import edu.isel.pdm.memorymatrix.MemoryMatrixApplication
+import edu.isel.pdm.memorymatrix.R
+import edu.isel.pdm.memorymatrix.utils.BaseActivity
+
+class ItemViewHolderLegacy(itemView: View) {
+    val topText: TextView = itemView.findViewById(android.R.id.text1)
+    val bottomText: TextView = itemView.findViewById(android.R.id.text2)
+}
+
+/**
+ * Adapter used to fill the list view items with the information from the data source.
+ */
+class HistoryAdapterLegacy(ctx: Context, history: List<GameResult>) :
+    ArrayAdapter<GameResult>(ctx, android.R.layout.simple_list_item_2, history.toTypedArray()) {
+
+    private val application = context.applicationContext as MemoryMatrixApplication
+    private val topTextPlaceHolder: String = ctx.resources.getString(R.string.history_item_score)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        Log.v(application.appTag, "converting view? ${convertView != null}")
+        val itemView = convertView ?: LayoutInflater.from(context)
+            .inflate(android.R.layout.simple_list_item_2, parent, false)
+
+        val gameResult = getItem(position) ?: throw IllegalStateException()
+
+        itemView.tag = itemView.tag ?: ItemViewHolderLegacy(itemView)
+
+        val viewHolder = itemView.tag as ItemViewHolderLegacy
+        viewHolder.topText.text = String.format(topTextPlaceHolder, gameResult.score.toString())
+        viewHolder.bottomText.text = gameResult.date.toString()
+
+        return itemView
+    }
+}
+
+/**
+ * Screen used to display the game scores' history using a List View.
+ */
+class HistoryActivityLegacy : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_legacy)
+
+        val history = findViewById<ListView>(R.id.historyList)
+        val viewModel: HistoryViewModelLegacy by viewModels()
+
+        viewModel.results.observe(this) {
+            history.adapter = HistoryAdapterLegacy(this, it)
+        }
+    }
+}
